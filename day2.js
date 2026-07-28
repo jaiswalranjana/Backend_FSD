8f3bae0d419a188ae7ed91ff8d021c97f566ad0a
jaiswalranjana <ranjana.25b01010698@abes.ac.in>
2026-07-28 09:18:22 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,53 @@
+//synchronous : blocking  js and asynchronous  : non blocking js 
+//synchronous fxn 
+// console.log("task3");
+// function hello(){
+//     console.log("task1");
+// }
+
+// hello();
+// console.log("task2"); 
+//to run use command -- node file name
+
+
+//asynchronous 
+//callback -- ? pass fxn as parameter in a fxn
+// function hello(){
+//     console.log("task1");
+//     setTimeout(function(){
+//         console.log("task2");
+//         console.log("task4");
+//     },2000)  //here sec r in milisec
+
+// }
+
+// hello();
+// console.log("task3");
+
+// function hello(n1,n2,callback){
+//     console.log("task1");
+//     return n1+n2;
+// }
+
+// let a=10;
+// let b=20;
+// console.log(hello(a,b,hi()));
+
+// function hi(){
+//     console.log("sayhiBy");
+// }
+
+
+
+// function demo(){
+//     console.log("demo");
+// }
+
+// demo();
+
+
+function display(){
+    console.log("learning fsd");
+}
+
+display();
