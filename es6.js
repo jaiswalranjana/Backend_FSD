5c3ade474ce2d983c64e71ed55d337b27a404622
jaiswalranjana <ranjana.25b01010698@abes.ac.in>
2026-07-28 09:08:54 +0530

    day1

---
+++ es6.js
@@ -0,0 +1,31 @@
+//java script
+//fxn : block of codes
+//syntax
+//function fname(){
+//block of code
+// }
+
+//function calling {}
+
+
+function sayHello(m){
+console.log(m,"hie Ranjana")
+console.log(arguments) //not possible in arrow fxn 
+
+}
+ function sum(a,b){
+    console.log(a+b)
+ }
+
+
+sayHello("Hey,");
+sum(3,6);
+
+//arrow fxn 
+//()=>{ }
+
+    const say=()=>{
+        console.log("wow arrow fxn")
+    }
+
+    say();
