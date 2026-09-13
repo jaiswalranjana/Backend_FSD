cb039e3a2d2ad708c0423c001e29caa4dfba046f
jaiswalranjana <ranjana.25b01010698@abes.ac.in>
2026-09-13 19:59:23 +0530

    Prg1

--- eventModules/program1.js
+++ eventModules/program1.js
@@ -40,15 +40,32 @@
 // event.emit("greet");
 // event.emit("greet");
 //Program 1:create custom EventEmitter that trigger "greet" or "exit"
-class MyEmitter extends EventEmitter{}
-const event=new MyEmitter()
-event.on("greet",(name)=>{
-console.log(`hello ${name}`);//template literals`${}`
+// class MyEmitter extends EventEmitter{}
+// const event=new MyEmitter()
+// event.on("greet",(name)=>{
+// console.log(`hello ${name}`);//template literals`${}`
+
+// })
+// event.on("exit",()=>{
+//     console.log("exits mycustom event emitter..");
+    
+// })
+// event.emit("greet","cse24");
+// event.emit("exit");
+
+
+//What is event
+//EventEmitter- on(emit event parameter, callback)- resgister event, event listener; emit(event parameter)- trigger/create/fire event
+
+const EventEmitter= require('events');
+class MyEvent extends EventEmitter{}
+const events = new EventEmitter();
+events.on("greet", (name)=>{
+    console.log(`Hello, ${name}! This is event emitter`);  //template literal ${variable}
 
 })
-event.on("exit",()=>{
-    console.log("exits mycustom event emitter..");
-    
-})
-event.emit("greet","cse24");
-event.emit("exit");
+events.on("exit", ()=>{
+    console.log("This is exit event");
+});
+events.emit("greet","Ranjana");
+events.emit("exit");
