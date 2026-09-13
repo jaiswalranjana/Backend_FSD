b0b08a960b0e749229014e8893da9c761edb8e14
jaiswalranjana <ranjana.25b01010698@abes.ac.in>
2026-09-13 20:03:07 +0530

    updatedIndexFileForExpress

--- express/index.js
+++ express/index.js
@@ -1,36 +1,79 @@
-import express from 'express';
-import fs from 'fs';
+// import express from 'express';
+// import fs from 'fs';
 
+// const app = express();
+// const PORT = 3000;
+// app.get('/', (req, res) => {
+//     fs.readFile('about.html', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Error reading file');
+//         } else {
+//             res.setHeader('Content-Type', 'text/html');
+//             res.send(data);
+//         }
+//     });
+// });
+// app.get('/about', (req, res) => {
+//     fs.readFile('about.html', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Error reading file');
+//         } else {
+//             res.send(data);
+//         }
+//     });
+// });
+// app.get('/contact', (req, res) => {
+//     fs.readFile('contact.html', (err, data) => {
+//         if (err) {
+//             res.status(500).send('Error reading file');
+//         } else {
+//             res.send(data);
+//         }
+//     });
+// });
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+import express from "express";
+import fs from "fs";
 const app = express();
 const PORT = 3000;
-app.get('/', (req, res) => {
-    fs.readFile('about.html', (err, data) => {
+
+app.get("/",(req, res) => {
+    fs.readFile("./pages/index.html", "utf-8", (err, data) => {
         if (err) {
-            res.status(500).send('Error reading file');
-        } else {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(data);
-        }
-    });
-});
-app.get('/about', (req, res) => {
-    fs.readFile('about.html', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading file');
+            res.status(500).send("Error reading the file.");
+            return;
         } else {
             res.send(data);
         }
     });
 });
-app.get('/contact', (req, res) => {
-    fs.readFile('contact.html', (err, data) => {
+
+app.get("/contact",(req, res) => {
+    fs.readFile("./pages/contact.html", "utf-8", (err, data) => {
         if (err) {
-            res.status(500).send('Error reading file');
+            res.status(500).send("Error reading the file.");
+            return;
         } else {
             res.send(data);
         }
     });
 });
+
+app.get("/about",(req, res) => {
+    fs.readFile("./pages/about.html", "utf-8", (err, data) => {
+        if (err) {
+            res.status(500).send("Error reading the file.");
+            return;
+        } else {
+            res.send(data);
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
